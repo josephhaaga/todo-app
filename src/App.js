f93041ff61fd06362ba679c80971297d9ae3a5f2
Joseph Haaga <haaga.joe@gmail.com>
2019-03-12 17:06:44 -0400

    added Material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button, List, ListItem } from './Components';
+import { List, ListItem } from './Components';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 import * as todoActions from './actions/todoActions';
+
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
 
 
 
@@ -16,11 +31,36 @@ class App extends Component {
       )
       return (
         <div className="App">
-        <Button onClick={e => this.props.todoActions.fetchTodos()}>Load</Button>
-        <Button onClick={e => this.props.todoActions.clearTodos()}>Clear</Button>
-        <List>
-          {todos}
-        </List>
+          <AppBar position="static" color="default">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">
+                Dashboard
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Grid container spacing={16}>
+            <Grid item xs={12} md={6}>
+              <Typography variant="h6" color="inherit">
+                Todos
+              </Typography>
+              <Button onClick={e => this.props.todoActions.fetchTodos()}>Load</Button>
+              <Button onClick={e => this.props.todoActions.clearTodos()}>Clear</Button>
+              <List>
+                {todos}
+              </List>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography variant="h6" color="inherit">
+                Todos
+              </Typography>
+              <Button onClick={e => this.props.todoActions.fetchTodos()}>Load</Button>
+              <Button onClick={e => this.props.todoActions.clearTodos()}>Clear</Button>
+              <List>
+                {todos}
+              </List>
+            </Grid>
+          </Grid>
+          
       </div>
     );
   }
